621c34f1542c7fb332e66c3555d54223800cedfa
Douwe Krooshof <dkrooshof@users.noreply.github.com>
2021-07-31 13:05:52 +0200

    Update Shutter_Timer_v1.2.ino
    I made this code to sheck the shutter of a mechanical slr with a horizontally travelling focal plane shutter. It calculates curtain speeds as well as relative exposure across the frame with a set-up using two laser diodes and two laser sensors.

--- Shutter_Timer_v1.2.ino
+++ Shutter_Timer_v1.2.ino
@@ -1,55 +1,115 @@
-long Start;   // this is the time in microseconds that the shutter opens (the arduino runs a microsecond clock in the background always - it is reasonably accurate for this purpose)
-long Stop;    // this is the time in microseconds that the shutter closes
-int Fired = 0;  // this is a flag indicating when the shutter has been fired completely.  when fired =1, the shutter has been fired, and the computer needs to display the information related to the exposure time.
-int Risingflag = 0;  // this is a flag that i set in my interrupt routine, Rising flag is set to = 1 when the voltage INCREASES in the interrupt
-int Fallingflag = 0;  // this is a flag that i set in the interrupt routine, Fallingflag is set to =1 when the voltage DECREASES in the interrupt
 
+//Douwe Krooshof July 2021, building on Cameradactyl's shutter tester code.
+//This is shutter tester for horizontally travelling focal plane shutters.
+//The tester uses two lasers and sensors to measure the speed of the curtains
+//and the relative exposure across the frame.
 
+int SensorDist = 25;                  // milimeter distance between the laser beams at the shutter plane.
 
-void setup() {                                                  //This part of the program is run exactly once on boot
+// set-up of variables for timing
+long StartTimeCurt1; 
+long StopTimeCurt1;
+long StartTimeCurt2; 
+long StopTimeCurt2;
 
-  Serial.begin(9600);                                          //opens a serial connection.
-  attachInterrupt(digitalPinToInterrupt(2), CLOCK, CHANGE);    //run the function CLOCK, every time the voltage on pin 2 changes.
+// set-up of logical parameters
+int Fired = 0;
+int RisingFlagSensor1 = 0;  
+int RisingFlagSensor2 = 0;
+int FallingFlagSensor1 = 0; 
+int FallingFlagSensor2 = 0;
 
+void setup() {                                                 
+  Serial.begin(9600);
+  Serial.print("Starting");  
+  Serial.println();   
+  // interrupts change logical parameters when voltage of the sensors changes in either direction.                                   
+  attachInterrupt(digitalPinToInterrupt(2), CLOCKSENSOR1, CHANGE);   
+  attachInterrupt(digitalPinToInterrupt(3), CLOCKSENSOR2, CHANGE);
 }
 
-void loop() {                                                  // this part of the program is run, in order, over and over again, start to finish, unless INTERRUPTED by our interrupt
-  if(Risingflag ==1){                       
-    Start = micros();       //set the variable Start to current microseconds
-  Risingflag=0;           //reset the rising flag to 0, so that this function isnt called again until the shutter actually fires
+void loop() { 
+  //curtain 1                                                
+  if(RisingFlagSensor1 == 1){        // Curtain 1 opening the shutter, sensor 1 exposed to laser 1, voltage goes up.               
+    StartTimeCurt1 = micros();       // set the variable StartTimeCurt1 to current microseconds 
+    RisingFlagSensor1 = 0;           // reset the RisingFlagSensor1 flag to 0, so that this function isnt called again until the shutter actually fires
   }
-  if(Fallingflag == 1){
-  Stop = micros();      // set the variable Stop to current microseconds
-  Fallingflag = 0;      //reset the falling flag to 0, so that this function isnt called again untill the shutter fires again.
-  Fired = 1;            // set the fired flag to 1, triggering the calculation of a shutter speed, and its display over the serial monitor.
+  if(RisingFlagSensor2 == 1){        // Curtain 1 still opening the shutter, sensor 2 exposed to laser 2, voltage goes up.
+    StopTimeCurt1 = micros();        // set the variable Stop to current microseconds
+    RisingFlagSensor2 = 0;           // reset the RisingFlagSensor2 to 0
   }
-  if(Fired == 1){                            //if the flag Fired = 1, print this information to the serial monitor"
-    Serial.print("Start: ");
-    Serial.println(Start);
-    Serial.print("Stop: ");
-    Serial.println(Stop);
-    long Speed = (Stop - Start);      // make a variable called speed, which is the total number of microseconds that the shutter is open for
-    Serial.print("Microseconds: ");
-    Serial.println(Speed);               //display total microseconds in shutter interval
   
+  //curtain 2
+  if(FallingFlagSensor1 == 1){       // Curtain 2 closing the shutter, sensor 1 covered, voltage goes down.               
+    StartTimeCurt2 = micros();       // set the variable StartTimeCurt2 to current microseconds
+    FallingFlagSensor1 = 0;          // reset the FallingFlagSensor1 flag to 0
+  }                      
+  if(FallingFlagSensor2 == 1){       // Curtain 2 still closing the shutter, sensor 2 covered, voltage goes down.
+    StopTimeCurt2 = micros();        // set the variable StopTimeCurt2 to current microseconds
+    FallingFlagSensor2 = 0;          // reset the FallingFlagSensor2 to 0.
+    Fired = 1;                       // set the fired flag to 1, triggering calculation and display.
+  }
 
-    float SS = (float)Speed/1000000;    // make a variable SS, which is how many seconds that the shutter open for
-    float SS2 = 1/SS;                   // make a variable SS2, which is the inverse of the SS, or 1/ the shutter speed
-    Serial.print("shutter speed: 1/");
-    Serial.println(SS2);                //display the shutter speed
-    Serial.println();
-    Start = 0;                         // reset Start to 0
-    Stop = 0;                           //reset Stop to 0 . *** these are not necessarily needed, but makes errors more evident should they occur
-    Fired = 0;                          //reset Fired flag to 0, so that the shutter speed will not be calclulated and displayed, until the next full interrupt cycle, where a start and stop time are generated.
+  
+  if(Fired == 1){                    // if the flag Fired = 1, print this information to the serial monitor"
+    // Curtain travel times:
+    long TravelTimeCurt1 = (StopTimeCurt1 - StartTimeCurt1);     
+    long TravelTimeCurt2 = (StopTimeCurt2 - StartTimeCurt2);     
+
+    // Curtain speed in meter per second
+    float Curt1Speed = (float)(SensorDist*1000)/TravelTimeCurt1;
+    float Curt2Speed = (float)(SensorDist*1000)/TravelTimeCurt2;
+
+    // Time for which the shutter is open at sensor 1 and 2 in microseconds
+    long OpenTimeAtSensor1 = (StartTimeCurt2 - StartTimeCurt1);
+    long OpenTimeAtSensor2 = (StopTimeCurt2 - StopTimeCurt1);
+
+    // Time for which the shutter is open at sensor 1 and 2 in seconds
+    float Sens1SS = (float)OpenTimeAtSensor1/1000000;
+    float Sens2SS = (float)OpenTimeAtSensor2/1000000;
+
+    // Inverse of the opening time in seconds for display
+    float InvSens1SS = 1/Sens1SS; 
+    float InvSens2SS = 1/Sens2SS;
+
+    Serial.print("Shutter speed at sensor 1: 1/");
+    Serial.println(InvSens1SS);             
+
+    Serial.print("Shutter speed at sensor 2: 1/");
+    Serial.println(InvSens2SS);             
+
+    Serial.print("First curtain speed (m/s): ");
+    Serial.println(Curt1Speed);
+
+    Serial.print("Second curtain speed (m/s): ");
+    Serial.println(Curt2Speed);
+
+    // reset
+    StartTimeCurt1 = 0; 
+    StopTimeCurt1 = 0;
+    StartTimeCurt2 = 0; 
+    StopTimeCurt2 = 0;
+    Serial.print("Shutter cycle completed!");
+    Serial.println();   
+    Serial.println();                       
+    Fired = 0;                          
   } 
 }
 
-void CLOCK(){                     //this is the interrupt function, which is called everytime the voltage on pin 2 changes, no matter where in the main program loop that the computer is currently in
+void CLOCKSENSOR1(){                     // interrupt function for sensor 1 on digital pin 2
   if(digitalRead(2) == HIGH){
-    Risingflag = 1;                // if the voltage on pin 2 is high, set the Risingflag to 1 : this will trigger the function called Rising from the main loop, which will set a start time
-  }
-  if(digitalRead(2) == LOW){        // . if the voltage on pin 2 is low, set the Fallingflag to 1 : this will trigger the function called Falling from the main loop, which will set the stop time, and also set the Fired flag to 1.
-    Fallingflag =1;
+    RisingFlagSensor1 = 1; 
+  }         
+  if(digitalRead(2) == LOW){        
+    FallingFlagSensor1 = 1;
   }
 }
 
+void CLOCKSENSOR2(){                     // interrupt function for sensor 2 on digital pin 3
+  if(digitalRead(3) == HIGH){
+    RisingFlagSensor2 = 1;               
+  }
+  if(digitalRead(3) == LOW){        
+    FallingFlagSensor2 =1;
+  }
+}
